63c6a6a7d3f3899666574a60b92fefe266301153
bungaayuss <bungaayu1049@gmail.com>
2025-05-25 22:02:32 +0700

    Tugas6 Laravel

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,7 +11,20 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
-Route::apiResource('/books', BookController::class);
-Route::apiResource('/genres', GenreController::class);
-Route::apiResource('/authors', AuthorController::class);
+Route::apiResource('/genres', GenreController::class)->only(['index','show']);
+Route::apiResource('/authors', AuthorController::class)->only(['index','show']);
+Route::apiResource('/books', BookController::class)->only(['index','show']);
+
+Route::middleware(['auth:api'])->group(function() {
+
+    Route::middleware(['role:admin'])->group(function() {
+    Route::apiResource('/books', BookController::class)->only(['store','update','destroy']);
+    Route::apiResource('/genres', GenreController::class)->only(['store','update','destroy']);
+    Route::apiResource('/authors', AuthorController::class)->only(['store','update','destroy']);
+
+    });
+});
